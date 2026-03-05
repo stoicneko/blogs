6a0fee302c315d5669332b1052190abad3015c73
stoicneko <anchor1618@outlook.com>
2026-03-05 13:25:04 +0800

    Revert title and author back to StoicNeko (capitalized)

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,9 +3,9 @@ import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } fro
 export const theme: ThemeUserConfig = {
   // [Basic]
   /** Title for your website. Will be used in metadata and as browser tab title. */
-  title: "stoicneko's Blog",
+  title: "StoicNeko's Blog",
   /** Will be used in index page & copyright declaration */
-  author: 'stoicneko',
+  author: 'StoicNeko',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Stay hungry, stay foolish',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
